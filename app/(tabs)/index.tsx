15f127d106578c54bc058d5aefd1ec45b5150bff
hoangday185 <jarvietnguyen318@gmail.com>
2025-01-15 19:17:22 +0700

    update delete todo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { getTodos } from "@/api/todo";
 import { useEffect, useState } from "react";
+import { TextInput } from "react-native";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { ScrollView } from "react-native";
 import { DataTable } from "react-native-paper";
@@ -12,8 +13,10 @@ export interface Todo {
 
 export default function HomeScreen() {
 	const [isLoading, setLoading] = useState(true);
+	const [isUpdate, setIsUpdate] = useState<boolean>(false);
 	const [data, setData] = useState<Todo[]>([]);
-
+	const [name, setName] = useState<string>("");
+	const [idUpdate, setIdUpdate] = useState<string>("");
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -46,15 +49,98 @@ export default function HomeScreen() {
 			});
 	};
 
+	const handleAddTodo = () => {
+		//call api add
+		fetch("https://67656e1852b2a7619f5f8733.mockapi.io/test-api/todos", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name, describe: "" }),
+		})
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				}
+			})
+			.then((res) => {
+				setData([...data, res]);
+			});
+		setName("");
+	};
+
+	const handleTodoById = (id: string) => () => {
+		//call api get by id
+		setIsUpdate(true);
+		fetch(`https://67656e1852b2a7619f5f8733.mockapi.io/test-api/todos/${id}`)
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				}
+			})
+			.then((res) => {
+				setName(res.name);
+				setIdUpdate(res.id);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	const handleUpdateTodo = (id: string) => () => {
+		//call api update
+		fetch(`https://67656e1852b2a7619f5f8733.mockapi.io/test-api/todos/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name, describe: "" }),
+		})
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				}
+			})
+			.then((res) => {
+				setData(data.map((item) => (item.id === id ? res : item)));
+			});
+		setName("");
+		setIsUpdate(false);
+	};
+
 	return (
 		<ScrollView style={styles.container}>
 			<View style={styles.block_to_do_list}>
 				<Text style={styles.title}>To do list</Text>
+				<View
+					style={{
+						marginBottom: 10,
+						marginTop: 10,
+						display: "flex",
+						width: 300,
+						flexDirection: "row",
+						justifyContent: "center",
+						alignItems: "flex-start",
+					}}
+				>
+					<TextInput
+						style={styles.input_todo}
+						onChangeText={(text) => setName(text)}
+						value={name}
+					/>
+					<View style={{ width: 90, marginLeft: 10 }}>
+						{isUpdate ? (
+							<Button title="Update" onPress={handleUpdateTodo(idUpdate)} />
+						) : (
+							<Button title="Add" onPress={handleAddTodo} />
+						)}
+					</View>
+				</View>
 				<View style={styles.list_to_do}>
 					<DataTable>
 						<DataTable.Header style={styles.tableHeader}>
 							<DataTable.Title>Name</DataTable.Title>
-							<DataTable.Title>Delete</DataTable.Title>
+							<DataTable.Title>Action</DataTable.Title>
 						</DataTable.Header>
 
 						{data.map((item) => (
@@ -65,6 +151,13 @@ export default function HomeScreen() {
 										color={"red"}
 										onPress={handleDeleteTodo(item.id)}
 										title="Delete"
+									/>
+								</DataTable.Cell>
+								<DataTable.Cell>
+									<Button
+										color={"green"}
+										onPress={handleTodoById(item.id)}
+										title="Update"
 									/>
 								</DataTable.Cell>
 							</DataTable.Row>
@@ -118,5 +211,19 @@ const styles = StyleSheet.create({
 	},
 	btn_delete: {
 		color: "red",
+	},
+	input_todo: {
+		outline: "none",
+		borderColor: "black",
+		borderWidth: 1,
+		borderStyle: "solid",
+		borderRadius: 5,
+		width: 200,
+		height: 40,
+		fontSize: 15,
+		paddingLeft: 5,
+	},
+	btnAdd: {
+		width: 90,
 	},
 });
